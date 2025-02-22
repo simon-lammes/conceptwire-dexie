c2943d3a4f22f93ffb9c4a17e74c9a5a2352dfcd
Simon Lammes <ghostsgandi@gmail.com>
2025-02-22 16:06:35 +0100

    wip

--- components/node-editors/image-node-editor.tsx
+++ components/node-editors/image-node-editor.tsx
@@ -1,5 +1,18 @@
 import type { ImageNode } from "@/models/node";
-import { Card, CardContent, CardMedia } from "@mui/material";
+import { MoreVert } from "@mui/icons-material";
+import {
+	Card,
+	CardContent,
+	CardHeader,
+	CardMedia,
+	ListItemButton,
+	ListItemText,
+	Popover,
+} from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import type React from "react";
+import { useId } from "react";
+import { useState } from "react";
 import { useDropzone } from "react-dropzone";
 
 export const ImageNodeEditor = ({
@@ -29,6 +42,10 @@ export const ImageNodeEditor = ({
 
 	return (
 		<Card variant="outlined" {...getRootProps({ className: "dropzone" })}>
+			<CardHeader
+				title="Image"
+				action={<MoreButton onRemove={onNodeRemoved} />}
+			/>
 			{node.blob && (
 				<CardMedia
 					component="img"
@@ -46,3 +63,46 @@ export const ImageNodeEditor = ({
 		</Card>
 	);
 };
+
+function MoreButton({ onRemove }: { onRemove: () => void }) {
+	const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+
+	const open = Boolean(anchorEl);
+	const popoverId = useId();
+
+	return (
+		<div>
+			<IconButton
+				aria-describedby={open ? popoverId : undefined}
+				onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
+					event.stopPropagation();
+					setAnchorEl(event.currentTarget);
+				}}
+			>
+				<MoreVert />
+			</IconButton>
+			<Popover
+				id={popoverId}
+				open={open}
+				anchorEl={anchorEl}
+				onClose={() => {
+					setAnchorEl(null);
+				}}
+				anchorOrigin={{
+					vertical: "bottom",
+					horizontal: "left",
+				}}
+			>
+				<ListItemButton component="button">
+					<ListItemText
+						onClick={(event) => {
+							event.stopPropagation();
+							onRemove();
+						}}
+						primary="Remove"
+					/>
+				</ListItemButton>
+			</Popover>
+		</div>
+	);
+}
