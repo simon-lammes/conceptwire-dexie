25ae412fcf0bf4301cff5d02b49c99b46b8cc7d6
Simon Lammes <ghostsgandi@gmail.com>
2025-02-22 14:24:19 +0100

    wip

--- components/node-views/markdown-view.tsx
+++ components/node-views/markdown-view.tsx
@@ -1,9 +1,16 @@
 import type { MarkdownNode } from "@/models/node";
 import Markdown from "react-markdown";
+import rehypeSanitize from "rehype-sanitize";
 
 export const MarkdownView = ({ node }: { node: MarkdownNode }) => {
 	return (
 		<Markdown
+			rehypePlugins={[
+				// I am not sure whether this sanitation is needed for security.
+				// Even without __dangerouslySetInnerHtml, there can be XSS vulnerabilities:
+				// https://github.com/facebook/react/issues/12441
+				rehypeSanitize,
+			]}
 			components={{
 				ul: (props) => (
 					<ul {...props} style={{ listStylePosition: "inside" }} />
