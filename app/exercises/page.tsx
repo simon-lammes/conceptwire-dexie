f75a743fff25ff9e9d6557591d97c1cb49b28e33
Simon Lammes <ghostsgandi@gmail.com>
2025-02-26 21:03:40 +0100

    wip

--- app/exercises/page.tsx
+++ app/exercises/page.tsx
@@ -1,19 +1,24 @@
 "use client";
 
+import { db } from "@/utils/db";
 import MenuIcon from "@mui/icons-material/Menu";
+import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import { useLiveQuery } from "dexie-react-hooks";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function ExercisesPage() {
 	const router = useRouter();
+	const exercises = useLiveQuery(() => db.exercises.toArray(), []);
 	return (
-		<Box sx={{ flexGrow: 1 }}>
-			<AppBar position="static">
+		<>
+			<AppBar position="sticky">
 				<Toolbar>
 					<IconButton
 						size="large"
@@ -37,6 +42,20 @@ export default function ExercisesPage() {
 					</Button>
 				</Toolbar>
 			</AppBar>
-		</Box>
+			<Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+				<List>
+					{exercises?.map((exercise) => (
+						<ListItem key={exercise.id} disablePadding>
+							<ListItemButton
+								component={Link}
+								href={`/exercises/${exercise.id}/edit`}
+							>
+								<ListItemText primary={exercise.id} />
+							</ListItemButton>
+						</ListItem>
+					))}
+				</List>
+			</Box>
+		</>
 	);
 }
