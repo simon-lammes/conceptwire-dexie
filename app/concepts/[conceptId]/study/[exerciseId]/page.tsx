2bfc42a2887e65b5ef28004204a580c9bf4472a3
Simon Lammes <ghostsgandi@gmail.com>
2025-03-05 22:22:43 +0100

    wip

--- app/concepts/[conceptId]/study/[exerciseId]/page.tsx
+++ app/concepts/[conceptId]/study/[exerciseId]/page.tsx
@@ -1,3 +1,36 @@
-export default function StudyPage() {
-	return <div>hi</div>;
+"use client";
+import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
+import { use } from "react";
+import { ArrowBack } from "@mui/icons-material";
+import Link from "next/link";
+import { useConcept } from "@/hooks/use-concept";
+
+export default function StudyPage({
+	params,
+}: {
+	params: Promise<{ conceptId: string }>;
+}) {
+	const { conceptId } = use(params);
+	const concept = useConcept(conceptId);
+
+	return (
+		<AppBar position="sticky">
+			<Toolbar>
+				<IconButton
+					size="large"
+					edge="start"
+					color="inherit"
+					aria-label="back"
+					sx={{ mr: 2 }}
+					component={Link}
+					href={`/concepts/${conceptId}`}
+				>
+					<ArrowBack />
+				</IconButton>
+				<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+					Study {concept?.title}
+				</Typography>
+			</Toolbar>
+		</AppBar>
+	);
 }
