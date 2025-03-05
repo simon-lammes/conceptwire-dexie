ff1dbd727d6142739783e4e11072b0b678b6b200
Simon Lammes <ghostsgandi@gmail.com>
2025-03-05 22:24:39 +0100

    wip

--- app/concepts/[conceptId]/study/[exerciseId]/page.tsx
+++ app/concepts/[conceptId]/study/[exerciseId]/page.tsx
@@ -1,36 +1,50 @@
 "use client";
-import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
+import {
+	AppBar,
+	Container,
+	IconButton,
+	Toolbar,
+	Typography,
+} from "@mui/material";
 import { use } from "react";
 import { ArrowBack } from "@mui/icons-material";
 import Link from "next/link";
 import { useConcept } from "@/hooks/use-concept";
+import { NodeView } from "@/components/node-view";
+import { useExercise } from "@/hooks/exercises/use-exercise";
 
 export default function StudyPage({
 	params,
 }: {
-	params: Promise<{ conceptId: string }>;
+	params: Promise<{ conceptId: string; exerciseId: string }>;
 }) {
-	const { conceptId } = use(params);
+	const { conceptId, exerciseId } = use(params);
 	const concept = useConcept(conceptId);
+	const exercise = useExercise(exerciseId);
 
 	return (
-		<AppBar position="sticky">
-			<Toolbar>
-				<IconButton
-					size="large"
-					edge="start"
-					color="inherit"
-					aria-label="back"
-					sx={{ mr: 2 }}
-					component={Link}
-					href={`/concepts/${conceptId}`}
-				>
-					<ArrowBack />
-				</IconButton>
-				<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-					Study {concept?.title}
-				</Typography>
-			</Toolbar>
-		</AppBar>
+		<>
+			<AppBar position="sticky">
+				<Toolbar>
+					<IconButton
+						size="large"
+						edge="start"
+						color="inherit"
+						aria-label="back"
+						sx={{ mr: 2 }}
+						component={Link}
+						href={`/concepts/${conceptId}`}
+					>
+						<ArrowBack />
+					</IconButton>
+					<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+						Study {concept?.title}
+					</Typography>
+				</Toolbar>
+			</AppBar>
+			<Container sx={{ py: 4 }}>
+				{exercise?.root ? <NodeView node={exercise.root} /> : undefined}
+			</Container>
+		</>
 	);
 }
