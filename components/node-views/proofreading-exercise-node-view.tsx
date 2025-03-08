dd0c7d19cd6d595c4862f01d71df9f4655b4c372
Simon Lammes <ghostsgandi@gmail.com>
2025-03-08 22:20:31 +0100

    wip

--- components/node-views/proofreading-exercise-node-view.tsx
+++ components/node-views/proofreading-exercise-node-view.tsx
@@ -1,6 +1,7 @@
 import type { ProofreadingExerciseNode } from "@/models/nodes/proofreading-exercise-node";
 import { Box } from "@mui/material";
 import Typography from "@mui/material/Typography";
+import { DiffEditor } from "@monaco-editor/react";
 
 export const ProofreadingExerciseNodeView = ({
 	node,
@@ -10,9 +11,11 @@ export const ProofreadingExerciseNodeView = ({
 			<Typography variant="h6" component="div">
 				Correct the following text.
 			</Typography>
-			<Typography variant="body1" component="div">
-				{node.incorrectText}
-			</Typography>
+			<DiffEditor
+				height="12rem"
+				original={node.incorrectText}
+				modified={node.incorrectText}
+			/>
 		</Box>
 	);
 };
